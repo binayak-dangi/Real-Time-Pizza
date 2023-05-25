4eea12a185733392fe55aec0c2484bd29ee3fe2d
Binayak Dangi <binayakdangi@Binayaks-MacBook-Pro.local>
2023-05-25 13:03:01 +0545

    add to cart

---
+++ Brewfile
@@ -0,0 +1,5 @@
+tap "homebrew/bundle"
+tap "homebrew/core"
+tap "homebrew/services"
+tap "mongodb/brew"
+brew "mingw-w64"
--- server.js
+++ server.js
@@ -1,34 +1,71 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-const expressLayout = require('express-ejs-layouts');
+const ejs = require("ejs");
+const path = require("path");
+const PORT = process.env.PORT || 4000;
+const expressLayout = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
+
+
+
+
+// database connection
+// const db = "mongodb://localhost:27017";
+// const url = "mongodb://localhost:27017/pizza";
+const url = "mongodb+srv://test:test12@cluster0.fl6tpu4.mongodb.net/pizza"
+mongoose.connect(url).then(() => {
+    console.log("Database Connected....");
+});
+
+// const connection = mongoose.connection;
+// connection.on("error", console.error.bind(console, "Connection Failed..."));
+// connection.once("open", function() {
+//     console.log("Database Connected...");
+// });
+
+// session store
+let mongoStore = MongoDbStore.create({
+
+    mongoUrl: url,
+    // ttl: 14 * 24 * 60 * 60, // = 14 days. Default
+});
+
+// session config
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        store: mongoStore,
+        saveUninitialized: false,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 24hrs
+    })
+);
+
+app.use(flash());
 
 // Assets
-app.use(express.static('public'));
+app.use(express.static("public"))
+app.use(express.json())
+
+// Global middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
 
 // set template engine
 app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
 // routes
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/cart', (req, res) => {
-    res.render('customers/cart');
-})
-app.get('/login', (req, res) => {
-    res.render('auth/login');
-})
-app.get('/register', (req, res) => {
-    res.render('auth/register');
-})
-
-
+require("./routes/web")(app);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+    console.log(`Listening on port ${PORT}`);
 });
